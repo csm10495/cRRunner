d81d927f7f664807daf7e9ef20cce0aad3d160d2
csm10495 <csm10495@gmail.com>
2017-12-18 20:08:27 -0800

    Switch to event driven model

--- crrunner/crruner.py
+++ crrunner/crruner.py
@@ -14,117 +14,38 @@ import paramiko
 import stat
 import time
 
-STATUS_SUCCESS = 0
-STATUS_TIMEOUT = 1
-STATUS_CODES = {
-    STATUS_SUCCESS : 'Completed Successfully',
-    STATUS_TIMEOUT : 'Timeout',
-}
-
-class TimeoutError(Exception):
-    '''
-    Brief:
-        Used to denote a timeout
-    '''
-    pass
-
-class Result(object):
-    def __init__(self, statusCode, remoteReturnCode=None, stdout=None, stderr=None, exception=None):
-        '''
-        Brief:
-            init for Result object. The result is the result of a command executed remotely.
-                It includes the remote return code, stdout, stderr and if generated locally,
-                    a Python exception. Typically the Python exception would be for a timeout.
-        '''
-        self.statusCode = statusCode
-        self.remoteReturnCode = remoteReturnCode
-        self.stdout = stdout
-        self.stderr = stderr
-        self.exception = exception
-
-    def getStatus(self):
-        '''
-        Brief:
-            Parses the statusCode as a string with description
-        '''
-        return '%d - %s' % (self.statusCode, STATUS_CODES.get(self.statusCode, 'Unknown'))
-
-    def didFail(self):
-        '''
-        Brief:
-            Returns True if the command failed due to something other than the command itself failing
-                For example: Timeout
-        '''
-        return self.statusCode != 0
-
-class CopyObject(object):
-    def __init__(self, local=None, remote=None):
-        '''
-        Brief:
-            Init for CopyObject. CopyObject is used to say this thing should be copyied here remotely
-
-        Argument(s):
-            local - (Optional; Defaults to None) - Location of local object (file or folder)
-                If None is given, will be placed/grabbed in/from the cwd
-            remote- (Optional; Defaults to None) - Location for this object on remote
-                If None is given, will be placed/grabbed in/from  in the cwd
-        '''
-        if local is None and remote is None:
-            raise ValueError('local and remote cannot both be None')
-
-        self.local = local
-        self.remote = remote
+from event import *
 
 class cRRunner(object):
-    def __init__(self, remoteIp, remoteCmd=None, remoteCmdTimeout=60, remoteUsername=None, remotePassword=None, copyObjectsTo=None, copyObjectsFrom=None, remotePort=22, cleanRemote=True, quiet=True):
+    def __init__(self, remoteIp, eventList, remoteUsername=None, remotePassword=None, remotePort=22, quiet=True):
         '''
         Brief:
             Configuration (and runner) for cRemote Runner
 
         Argument(s):
             remoteIp - (Required) - IP for remote SSH connection
-            remoteCmd - (Optional; Defaults to None) - Cmd to execute after copying all copyObjects
-                If None, won't use execute a command.
-            remoteCmdTimeout - (Optional; Defaults to 60) - Timeout for remoteCmd in seconds
+            eventList - (Required) - List of events to execute (in order)
             remoteUsername - (Optional; Defaults to None) - Text username for remote SSH connection
                 If None is given, will assume we don't need credentials.
             remotePassword - (Optional; Defaults to None) - Text password for remote SSH connection
                 If None is given, will assume we don't need credentials.
-            copyObjectsTo - (Optional; Derfaults to None) - List of CopyObject to copy to (before).
-                If None, nothing to copy
-            copyObjectsFrom - (Optional; Derfaults to None) - List of CopyObject to copy from (after)
-                If None, nothing to copy... TODO. NOT IMPLEMENTED YET.
             remotePort - (Optional; Defaults to 22) - Port for SSH connection
-            cleanRemote - (Optional; Defaults to True) - If True, delete copied files on remote
-                after copying objects from back.
             quiet - (Optional; Defaults to True) - If True, be quiet and don't log to screen
         '''
         self._sshClient = None
         self._sftpClient = None
-        self.quiet = quiet
-
-        self.remoteIp = remoteIp
-        self.remoteCmdTimeout = remoteCmdTimeout
 
         if type(remoteUsername) is not type(remotePassword):
             raise ValueError("If remoteUsername is provided, we also need remotePassword")
 
+        self.remoteIp = remoteIp
         self.remoteUsername = remoteUsername
         self.remotePassword = remotePassword # lol security
-
-        self.remoteCmd = remoteCmd
-
-        if copyObjectsTo is None:
-            copyObjectsTo = []
-
-        self.copyObjectsTo = copyObjectsTo
-
-        if copyObjectsFrom is None:
-            copyObjectsFrom = []
-
-        self.copyObjectsFrom = copyObjectsFrom
         self.remotePort = remotePort
-        self.cleanRemote = cleanRemote
+
+        self.eventList = eventList
+
+        self.quiet = quiet
 
     def _getSshClient(self):
         '''
@@ -152,16 +73,6 @@ class cRRunner(object):
         if self._sftpClient is None or self._sftpClient.get_channel().closed:
             self._sftpClient = self._getSshClient().open_sftp()
         return self._sftpClient
-
-    def _execute(self):
-        '''
-        Brief:
-            Executes the saved remoteCmd. Returns stdout, stderr
-        '''
-        stdout, stderr = self._raw_execute(self.remoteCmd, timeoutSeconds=self.remoteCmdTimeout)
-        retCode = stdout.channel.recv_exit_status()
-        r = Result(statusCode=STATUS_SUCCESS, remoteReturnCode=retCode, stdout=stdout.read().decode(), stderr=stderr.read().decode(), exception=None)
-        return r
 
     def _raw_execute(self, remoteCmd, timeoutSeconds):
         '''
@@ -246,34 +157,6 @@ class cRRunner(object):
         '''
         raise NotImplementedError
 
-    def _doCopyObjectsTo(self):
-        '''
-        Brief:
-            Goes through copyObjectsTo and copies all over to remote
-        '''
-        self._remoteToDelete = []
-        stfp = self._getSftpClient()
-        for copyObj in self.copyObjectsTo:
-            self._remoteToDelete.extend(self._put(copyObj.local, copyObj.remote))
-
-    def _doCopyObjectsFrom(self):
-        '''
-        Brief:
-            Goes through copyObjectsFrp, and copies all objectes here
-        '''
-        if len(self.copyObjectsFrom) != 0:
-            raise NotImplementedError("copyObjectsFrom is not implemented yet.")
-
-    def _doDeleteRemote(self):
-        '''
-        Brief:
-            Deletes all known (copied) remote files
-        '''
-        sftp = self._getSftpClient()
-        for i in self._remoteToDelete:
-            sftp.unlink(i)
-        self.log("Cleaning... Deleted %d remote files" % len(self._remoteToDelete))
-
     def log(self, s):
         '''
         Brief:
@@ -300,30 +183,30 @@ class cRRunner(object):
     def run(self):
         '''
         Brief:
-            Runs the CRRunner.
-                Copies objects to, executes, then copies objects from
+            Runs the CRRunner. Runs all given Events. Returns all the results.
         '''
-        self._doCopyObjectsTo()
-        result = None
-        if self.remoteCmd is not None:
-            try:
-                result = self._execute()
-            except Exception as ex:
-                # remember we are passing the stderr/stdout with the exception
-                result = Result(statusCode=STATUS_TIMEOUT, exception=ex, stdout=ex.stdout.read().decode(), stderr=ex.stderr.read().decode())
-        self._doCopyObjectsFrom()
-        if self.cleanRemote:
-            self._doDeleteRemote()
+        self.results = []
+        for i in self.eventList:
+            r = i.run(self) # pass the runner to run
+            self.results.append(r)
 
         self.close()
 
-        return result
+        return self.results
 
 if __name__ == '__main__':
     # test code
     remoteIp = os.environ['REMOTE_IP']
     remotePassword = os.environ['REMOTE_PASSWORD']
 
-    copyTos = [CopyObject(r"C:\Users\csm10495\Desktop\Stuff\app")]
-
-    c = cRRunner(remoteIp=remoteIp, remoteUsername='test', remotePassword=remotePassword, remoteCmd='ls', copyObjectsTo=copyTos, quiet=False)
+    c = cRRunner(remoteIp=remoteIp, eventList=[
+        ExecuteEvent('ls'),
+        CopyToRemoteEvent(
+            [
+                CopyObject(r"C:\Users\csm10495\Desktop\Stuff\app")
+            ]
+        ),
+        ExecuteEvent('sleep 5', 2),
+        DeleteAllCopiedToRemote(),
+        ],
+    remoteUsername='test', remotePassword=remotePassword, quiet=False)
